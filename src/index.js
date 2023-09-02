fa63cb807f99e50644b67858e98d8d37b04dec93
Joseph <jvp.2703@gmail.com>
2023-09-02 19:11:48 +0100

    loader done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const searchForm = document.querySelector("#search-form");
 const filmList = document.querySelector("#film-list");
-
-//film-watchlist
-
-const watchListButton = document.querySelector("#film-watchlist");
+const loadingElement = document.querySelector("#loading");
 
 const filmsfromLocalStorage = JSON.parse(localStorage.getItem("watchlist"));
 
@@ -12,9 +9,7 @@ const key = import.meta.env.VITE_MOVIES_APIKEY;
 let resultArr = [];
 let watchListArr = [];
 
-
 function generateMovie(film, list) {
-  
   list.innerHTML += `
       <div id="film-item-container">
           <div id="film-item">
@@ -44,6 +39,14 @@ function generateMovie(film, list) {
   `;
 }
 
+function showLoading() {
+  filmList.innerHTML = `
+  <div id="loader-container">
+    <span id="loader"></span>
+  </div>
+  `
+}
+
 // WE OBTAIN THE imdbID from this first Fetch
 // Then we pass the function fetchMovies to each result of searchResult variable
 async function dataFetch(movie) {
@@ -63,12 +66,11 @@ async function dataFetch(movie) {
 }
 
 async function fetchMovies(result) {
-  // const loadingElement = document.querySelector('#loading')
-  // loadingElement.style.display = 'block'
+  filmList.innerHTML = "";
+  showLoading()
   const URL = `https://www.omdbapi.com/?apikey=${key}&i=${result.imdbID}`;
   const res = await fetch(URL);
   const data = await res.json();
-
 
   // console.log(data);
 
@@ -162,8 +164,8 @@ function handleAddFilm(filmID) {
   localStorage.setItem("watchlist", JSON.stringify(watchListArr));
 }
 
-filmList.innerHTML = `
-<div id="empty-state-container">
+ filmList.innerHTML = `
+  <div id="empty-state-container">
     <svg id="svg-icon" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="5em" width="5em" xmlns="http://www.w3.org/2000/svg"><path d="M18 4l2 3h-3l-2-3h-2l2 3h-3l-2-3H8l2 3H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4zm-6.75 11.25L10 18l-1.25-2.75L6 14l2.75-1.25L10 10l1.25 2.75L14 14l-2.75 1.25zm5.69-3.31L16 14l-.94-2.06L13 11l2.06-.94L16 8l.94 2.06L19 11l-2.06.94z"></path>
     </svg>
     <p>
